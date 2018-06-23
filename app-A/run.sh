0a7e470116e6b6210c7e1adffa123db97914b437
Kenta Shinohara <shinohara.kenta@lab.ntt.co.jp>
2018-06-24 02:00:28 +0900

    configure dstat (not work)

--- app-A/run.sh
+++ app-A/run.sh
@@ -4,8 +4,14 @@ TIME=600
 
 sed -i -e "s/TARGETHOST/$SVC_ESEARCH_PORT_9200_TCP_ADDR/" /etc/td-agent/td-agent.conf
 
-mkdir /home/log
-chmod 777 /home/log
+mkdir /home/log && chmod 777 /home/log
+
+for i in cpu base special; do touch /out.$i.csv.pos; chmod 777 /out.$i.csv.pos; done
+
+dstat -Tc -C total,0,1,2,3,4,5,6,7 --output /dstat.cpu.csv > /dev/null &
+dstat -Tmdgsnpry --output /dstat.base.csv > /dev/null &
+dstat -T --aio --fs --ipc --lock --raw --socket --tcp --udp --unix --vm --output /dstat.special.csv > /dev/null &
+
 
 sleep 10
 service td-agent start
